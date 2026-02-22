711ccd7d12b29cdc625cc02e4e3aeebf7c0b6ba4
Sumirui <124206372+UikaMisumi@users.noreply.github.com>
2026-02-22 01:23:05 -0600

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,11 @@ AUTH_COOKIE_NAME = "madnote_token"
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://mad-data-2026-mad-note-dcwy7y137-uikamisumis-projects.vercel.app", 
+        "http://localhost:5173", 
+        "http://127.0.0.1:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
